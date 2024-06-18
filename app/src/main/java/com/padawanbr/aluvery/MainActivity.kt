52f98d57c69f5a561046dd230dfa44c961ca80bb
brect <contato@blimas.dev>
2024-06-17 23:43:00 -0300

    feat: criando o item do produto

--- app/src/main/java/com/padawanbr/aluvery/MainActivity.kt
+++ app/src/main/java/com/padawanbr/aluvery/MainActivity.kt
@@ -3,19 +3,18 @@ package com.padawanbr.aluvery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.padawanbr.aluvery.ui.theme.AluveryTheme
@@ -26,93 +25,28 @@ class MainActivity : ComponentActivity() {
         setContent {
             AluveryTheme {
                 Surface {
-                    MyFirstComposable()
                 }
             }
-            MyFirstComposable()
         }
     }
 }
 
-@Composable
-fun MyFirstComposable() {
-    Text(text = "My first Composable")
-    Text(text = "My first content Composable")
-}
 
 @Preview(showBackground = true)
 @Composable
-fun CustomLayoutPreview() {
-    Column(
-        modifier = Modifier
-            .padding(all = 8.dp)
-            .background(color = Color.Blue)
-            .fillMaxWidth()
-            .fillMaxHeight()
-    ) {
-        Text(text = "Texto 1")
-        Text(text = "Texto 2")
-        Row(
-            modifier = Modifier
-                .padding(
-                    horizontal = 8.dp, vertical = 16.dp
-                )
-                .background(color = Color.Yellow)
-        ) {
-            Text(text = "Texto 3")
-            Text(text = "Texto 4")
-        }
+fun ProdutItem() {
+    Column {
         Box(
             modifier = Modifier
-                .padding(all = 8.dp)
-                .background(color = Color.Red)
-                .padding(all = 8.dp)
-        ) {
-            Row(
-                modifier = Modifier
-                    .padding(all = 8.dp)
-                    .background(color = Color.Cyan)
-                    .padding(all = 8.dp)
-            ) {
-                Text(text = "Texto 5")
-                Text(text = "Texto 6")
-            }
-            Column(
-                modifier = Modifier
-                    .padding(all = 8.dp)
-                    .background(color = Color.White)
-                    .padding(all = 8.dp)
-            ) {
-                Text(text = "Texto 7")
-                Text(text = "Texto 8")
-            }
-        }
-    }
-}
-
-@Preview
-@Composable
-fun ColumnPreview() {
-    Column {
+                .height(50.dp)
+                .background(Color.Blue)
+                .width(50.dp)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Imagem do produto"
+        )
         Text(text = "Texto 1")
-        Text(text = "Texto 1")
-    }
-}
-
-@Preview
-@Composable
-fun RowPreview() {
-    Row {
-        Text(text = "Texto 1")
-        Text(text = "Texto 1")
-    }
-}
-
-@Preview
-@Composable
-fun BoxPreview() {
-    Box {
-        Text(text = "Texto 1")
-        Text(text = "Texto 1")
+        Text(text = "Texto 2")
     }
 }
