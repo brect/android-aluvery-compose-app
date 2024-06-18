86f30f5cd15ba7d7d9e1823d79d19b1f488549dd
brect <contato@blimas.dev>
2024-06-17 23:20:17 -0300

    feat: personalizando com o Modifier

--- app/src/main/java/com/padawanbr/aluvery/MainActivity.kt
+++ app/src/main/java/com/padawanbr/aluvery/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.padawanbr.aluvery
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.padawanbr.aluvery.ui.theme.AluveryTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,20 +42,47 @@ fun MyFirstComposable() {
 
 @Preview(showBackground = true)
 @Composable
-fun CustomLayoutPreview(){
-    Column {
+fun CustomLayoutPreview() {
+    Column(
+        modifier = Modifier
+            .padding(all = 8.dp)
+            .background(color = Color.Blue)
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
         Text(text = "Texto 1")
         Text(text = "Texto 2")
-        Row {
+        Row(
+            modifier = Modifier
+                .padding(
+                    horizontal = 8.dp, vertical = 16.dp
+                )
+                .background(color = Color.Yellow)
+        ) {
             Text(text = "Texto 3")
             Text(text = "Texto 4")
         }
-        Box {
-            Row {
+        Box(
+            modifier = Modifier
+                .padding(all = 8.dp)
+                .background(color = Color.Red)
+                .padding(all = 8.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .padding(all = 8.dp)
+                    .background(color = Color.Cyan)
+                    .padding(all = 8.dp)
+            ) {
                 Text(text = "Texto 5")
                 Text(text = "Texto 6")
             }
-            Column {
+            Column(
+                modifier = Modifier
+                    .padding(all = 8.dp)
+                    .background(color = Color.White)
+                    .padding(all = 8.dp)
+            ) {
                 Text(text = "Texto 7")
                 Text(text = "Texto 8")
             }
