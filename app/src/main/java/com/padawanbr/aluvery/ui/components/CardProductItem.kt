c3b12272a4788f5e58abc0d3ed91c65af2be4573
brect <contato@blimas.dev>
2024-06-28 19:32:42 -0300

    feat: Adicionando a lista de cards de produto

---
+++ app/src/main/java/com/padawanbr/aluvery/ui/components/CardProductItem.kt
@@ -0,0 +1,82 @@
+package com.padawanbr.aluvery.ui.components
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.padawanbr.aluvery.R
+import com.padawanbr.aluvery.extensions.toBrazilianCurrency
+import com.padawanbr.aluvery.model.Product
+import com.padawanbr.aluvery.sampledata.sampleProducts
+import com.padawanbr.aluvery.ui.theme.AluveryTheme
+
+@Composable
+fun CardProductItem(
+    product: Product,
+    elevation: Dp = 4.dp
+) {
+    Card(
+        Modifier
+            .fillMaxWidth()
+            .heightIn(150.dp),
+        elevation = elevation
+    ) {
+        Column {
+            AsyncImage(
+                model = product.image,
+                contentDescription = null,
+                Modifier
+                    .fillMaxWidth()
+                    .height(100.dp),
+                placeholder = painterResource(id = R.drawable.placeholder),
+                contentScale = ContentScale.Crop
+            )
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colors.primaryVariant)
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = product.name
+                )
+                Text(
+                    text = product.price.toBrazilianCurrency()
+                )
+            }
+            // TODO: adicionar descrição do produto
+            // Text(
+            //     text = product.description,
+            //     Modifier
+            //         .padding(16.dp)
+            // )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun CardProductItemPreview() {
+    AluveryTheme {
+        Surface {
+            CardProductItem(
+                product = sampleProducts.random(),
+            )
+        }
+    }
+}
--- app/src/main/java/com/padawanbr/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/padawanbr/aluvery/ui/screens/HomeScreen.kt
@@ -5,9 +5,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,7 +25,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.padawanbr.aluvery.model.Product
+import com.padawanbr.aluvery.sampledata.sampleProducts
 import com.padawanbr.aluvery.sampledata.sampleSections
+import com.padawanbr.aluvery.ui.components.CardProductItem
 import com.padawanbr.aluvery.ui.components.ProductsSection
 import com.padawanbr.aluvery.ui.theme.AluveryTheme
 
@@ -30,10 +40,29 @@ fun HomeScreen(
         var text by remember {
             mutableStateOf("")
         }
-
-        OutlinedTextField(value = text, onValueChange = {
-            value -> text = value
-        })
+        OutlinedTextField(
+            value = text,
+            onValueChange = { value ->
+                text = value
+            },
+            modifier = Modifier
+                .padding(
+                    start = 16.dp,
+                    end = 16.dp,
+                    top = 16.dp
+                )
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(25),
+            leadingIcon = {
+                Icon(Icons.Default.Search, contentDescription = "ícone de lupa")
+            },
+            label = {
+                Text(text = "Produto")
+            },
+            placeholder = {
+                Text(text = "O que você procura?")
+            }
+        )
 
         LazyColumn(
             Modifier
@@ -41,20 +70,21 @@ fun HomeScreen(
             verticalArrangement = Arrangement.spacedBy(16.dp),
             contentPadding = PaddingValues(vertical = 16.dp)
         ) {
-            item { Spacer(modifier = Modifier) }
 
-            for (section in sections) {
-                val title = section.key
-                val products = section.value
-                item {
-                    ProductsSection(
-                        title = title,
-                        products = products
-                    )
-                }
+            items(sampleProducts) { p->
+                CardProductItem(product = p)
             }
+//            for (section in sections) {
+//                val title = section.key
+//                val products = section.value
+//                item {
+//                    ProductsSection(
+//                        title = title,
+//                        products = products
+//                    )
+//                }
+//            }
 
-            item { Spacer(modifier = Modifier) }
         }
     }
 
