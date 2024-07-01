17586e2b09225c9f3649a8da619ba2166fb83488
brect <contato@blimas.dev>
2024-06-30 23:53:49 -0300

    feat: Adicionando os campos do formulário

--- app/src/main/java/com/padawanbr/aluvery/activities/ProductFormActivity.kt
+++ app/src/main/java/com/padawanbr/aluvery/activities/ProductFormActivity.kt
@@ -3,10 +3,17 @@ package com.padawanbr.aluvery.activities
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import com.padawanbr.aluvery.sampledata.sampleSections
-import com.padawanbr.aluvery.ui.screens.HomeScreen
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.tooling.preview.Preview
 import com.padawanbr.aluvery.ui.theme.AluveryTheme
 
 class ProductFormActivity: ComponentActivity() {
@@ -14,20 +21,67 @@ class ProductFormActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ProductFormApp()
+            AluveryTheme {
+                Surface {
+                    ProductFormScreen()
+                }
+            }
         }
     }
-
 }
 
-
 @Composable
-fun ProductFormApp() {
+fun ProductFormScreen() {
+    Column {
+        Text(text = "Criando o produto")
+
+        var url by remember {
+            mutableStateOf("")
+        }
+
+        TextField(value = url, onValueChange = {
+            url = it
+        })
+
+
+        var name by remember {
+            mutableStateOf("")
+        }
+
+        TextField(value = name, onValueChange = {
+            name = it
+        })
+
+
+
+        var price by remember {
+            mutableStateOf("")
+        }
+
+        TextField(value = price, onValueChange = {
+            price = it
+        })
+
+        var description by remember {
+            mutableStateOf("")
+        }
+
+        TextField(value = description, onValueChange = {
+            description = it
+        })
+
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Salvar")
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun ProductFormScreenPreview() {
     AluveryTheme {
         Surface {
-            HomeScreen(
-                sampleSections
-            )
+            ProductFormScreen()
         }
     }
 }
